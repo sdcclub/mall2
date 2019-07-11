6a08ddc6aeba5883c904de99e3da0eb87530e85f
137 <1976886132@qq.com>
2019-07-11 17:00:19 +0800

    测试类有点问题

---
+++ src/main/java/test/PayServiceTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import com.etc.model.entity.Goods;
+import com.etc.model.entity.User;
+import com.etc.model.service.PayService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:Configuration.xml"})
+public class PayServiceTest {
+
+    PayService payService;
+
+    public PayService getPayService() {
+        return payService;
+    }
+
+    @Autowired
+    public void setPayService(PayService payService) {
+        this.payService = payService;
+    }
+
+    @Test
+    public void getPayList() {
+        User user=new User();
+        user.setUid(1);
+        System.out.println("asdfasdfsdf");
+        List<Goods> goodsList=payService.getPayList(user);
+        for(Goods goods:goodsList){
+            System.out.println(goods);
+        }
+    }
+}
