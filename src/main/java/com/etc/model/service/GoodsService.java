118e8298a37b3071d23fb0e0c46da84093296473
CarrieXtt <2978776326@qq.com>
2019-07-11 20:29:35 +0800

    生成shop.jsp，新建Goods的service和controller

---
+++ src/main/java/com/etc/model/service/GoodsService.java
@@ -0,0 +1,26 @@
+package com.etc.model.service;
+
+import com.etc.model.dao.GoodsMapper;
+import com.etc.model.entity.Goods;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GoodsService {
+    private GoodsMapper goodsMapper;
+
+    public GoodsMapper getGoodsMapper() {
+        return goodsMapper;
+    }
+
+    @Autowired
+    public void setGoodsMapper(GoodsMapper goodsMapper) {
+        this.goodsMapper = goodsMapper;
+    }
+
+    public List<Goods> findAll(){
+        return goodsMapper.selectByExample(null);
+    }
+}
