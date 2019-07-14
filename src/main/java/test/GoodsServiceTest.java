de112cf7bb1d17ce3578c7e6ec7dfe4cbf4798fe
CarrieXtt <2978776326@qq.com>
2019-07-14 13:35:38 +0800

    基本功能完成，但shop.jsp的界面还需要修改

---
+++ src/main/java/test/GoodsServiceTest.java
@@ -0,0 +1,56 @@
+package test;
+
+import com.etc.model.entity.Cart;
+import com.etc.model.entity.Goods;
+import com.etc.model.service.GoodsService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * @Author: Carrie Xie
+ * @Date: 2019/7/12 19:33
+ * @Version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath*:test/Configuration.xml","classpath*:test/springmvc-servlet.xml"})
+public class GoodsServiceTest {
+    GoodsService goodsService;
+
+    public GoodsService getGoodsService() {
+        return goodsService;
+    }
+
+    @Autowired
+    public void setGoodsService(GoodsService goodsService) {
+        this.goodsService = goodsService;
+    }
+
+    @Test
+    public void selectCartByUidAndGid() {
+        System.out.println("hhhh");
+        List<Cart> list = goodsService.selectCartByUidAndGid(1,1);
+        for (Cart cart:list) {
+            System.out.println(cart);
+        }
+    }
+
+    @Test
+    public void updateCcountByUidAndGid() {
+        System.out.println("hhhh");
+        goodsService.updateCcountByUidAndGid(1,1,4);
+    }
+
+    @Test
+    public void findByLike(){
+        String string = "子";
+        List<Goods> list = goodsService.findByLike(string);
+        for(Goods goods:list){
+            System.out.println(goods.getGname()+" ");
+        }
+    }
+}
