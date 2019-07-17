bda090c028c2da033e45e7e2b388cbe6c3e108e2
Jasminexyy <1907184421@qq.com>
2019-07-17 16:17:21 +0800

    单元测试 LoginService

---
+++ src/main/java/test/com/etc/model/service/BaseJunit4Test.java
@@ -0,0 +1,11 @@
+package test.com.etc.model.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"springmvc-servlet.xml"})
+public class BaseJunit4Test {
+
+}
